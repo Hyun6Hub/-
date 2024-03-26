da01357aa7c5598c093d684bc33dfdb190b2e6af
Hyun6Hub <sad765479@gmail.com>
2024-03-26 13:45:06 +0900

    커밋테스트

---
+++ components/Button.jsx
@@ -0,0 +1,5 @@
+//props = {text:'All'} => {props.text}
+//{text} = props       => {text}
+export default function Button({ text, bg }) {
+  return <Button style={{ backgroundcolor: "bg" }}>{props.text}</Button>;
+}
